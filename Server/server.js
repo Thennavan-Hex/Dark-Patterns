4993a949034016f54caa8ff9b9687b48a89b6181
Thennavan-Hex <thensthennavan3@gmail.com>
2024-01-22 22:28:12 +0530

    Remove node_modules from version control

--- Server/server.js
+++ Server/server.js
@@ -25,13 +25,10 @@ client.connect()
 app.post('/api/get-url-score', async (req, res) => {
   const { url } = req.body;
 
-  try {
-  
-    const parsedUrl = new URL(url);
-    const domain = parsedUrl.hostname;
+  console.log('Received URL:', url);
 
-    const result = await client.query('SELECT total_score FROM url_data WHERE url = $1', [url]);
-    const totalScore = result.rows.length > 0 ? result.rows[0].total_score : 0;
+  try {
+    const totalScore = await getUrlScore(url);
 
     console.log('Total score fetched successfully for URL:', url, 'Total Score:', totalScore);
     res.json({ totalScore });
@@ -40,6 +37,35 @@ app.post('/api/get-url-score', async (req, res) => {
     res.status(500).json({ status: 'error', message: 'Internal server error' });
   }
 });
+
+async function getUrlScore(url) {
+  try {
+    // Validate and correct the URL before creating a URL object
+    const correctedUrl = validateAndCorrectUrl(url);
+
+    if (!correctedUrl) {
+      console.error('Invalid URL:', url);
+      return 0; // Respond with 0% availability for invalid URLs
+    }
+
+    const result = await client.query('SELECT total_score FROM url_data WHERE url = $1', [correctedUrl]);
+    return result.rows.length > 0 ? result.rows[0].total_score : 0;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Function to validate and correct URL
+function validateAndCorrectUrl(inputUrl) {
+  try {
+    // Prepend "http://" if the URL doesn't have a scheme
+    const correctedUrl = inputUrl.startsWith('http') ? inputUrl : `http://${inputUrl}`;
+    new URL(correctedUrl);
+    return correctedUrl;
+  } catch (error) {
+    return null;
+  }
+}
 
 const PORT = 3000;
 app.listen(PORT, () => {
